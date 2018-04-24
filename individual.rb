6154dc9c159e00d59d8006ee64c30f3ee07f369f
Andy Sisinger <andocmdo@gmail.com>
2018-04-24 19:06:13 -0400

    add init and class load for evolved individual

--- individual.rb
+++ individual.rb
@@ -1,4 +1,26 @@
 
 class Individual
+  def initialize(conf)
+    # make our genome arrays
+    buy_genes = Array.new
+    sell_genes = Array.new
+    hold_genes = Array.new
+
+    # pull in the startingCash amount
+    starting_cash = conf["agent"]["startingCash"]
+
+    # Now load in the appropriate files/classes for genome
+    conf["agent"]["buyGenes"].each do |filename|
+      buy_genes << get_new_class_object_from_filename(filename)
+    end
+    conf["agent"]["sellGenes"].each do |filename|
+      sell_genes << get_new_class_object_from_filename(filename)
+    end
+    conf["agent"]["holdGenes"].each do |filename|
+      hold_genes << get_new_class_object_from_filename(filename)
+    end
+  # init end
   
+  end
+# class end
 end
--- pianoforte.rb
+++ pianoforte.rb
@@ -1,8 +1,22 @@
+require 'json'
 load "util.rb"
 load "ga.rb"
 
+# load configuration
+if ARGV.empty?
+  puts "Missing configuration argument. Need either JSON string, or filename!"
+elsif ARGV[0].start_with?("{")
+  config = JSON.parse(ARGV[0])
+else
+  config_file = ARGV[0]
+end
+
 # Create and setup our GA simulation
-ga = GA.new
+ga = GA.new(
+  population_size: config["ga"]["populationSize"],
+  mutation_rate: config["ga"]["mutationRate"],
+  xover_pool_size_ratio: config["ga"]["xoverPoolSizeRatio"]
+)
 puts "Created GA -> population: #{ga.population_size}, mutation rate: #{ga.mutation_rate},
   xover pool size ratio: #{ga.xover_pool_size_ratio}"
-ga.create_population(Individual.new)  # In the future we can choose the type of individual to evolve?
+ga.create_population(Individual.new(config))  # In the future we can choose the type of individual to evolve?
